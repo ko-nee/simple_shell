870c40da969495acc05ecf75b3811efc65041302
ko-nee <konemas10@gmail.com>
2023-10-18 14:19:11 -0700

    print_functions.c

---
+++ print_functions.c
@@ -0,0 +1,39 @@
+#include "shell.h"
+
+/**
+ *read_string - function that takes user input
+ *@string - input from the user entered on the interface
+ *@size - length of string in character count
+ *
+ */
+
+void read_string(char *string, side_t size);
+{
+	if (fgets(string, size, stdin) == NULL)
+	{
+		if (feof(stdin))
+		{
+		print_out("\n");
+		exit(EXIT_SUCCESS);
+		}
+		else
+		{
+		print_out("Error while reading input.\n");
+		exit(EXIT_FAILURE);
+		}
+	}
+	string[strcspn(string, "\n")] = '\0';
+}
+
+#include "shell.h"
+
+/**
+ *print_out - creates a function that can print to the standard output
+ *@string - output printed out onto the interface
+ *
+ */
+
+void print_out(const char *string)
+{
+	write(STDOUT_FILENO, string, strlen(string));
+}
